93e2036404dfe661831a33ba51f0ccfd27596c06
Rin Minase <rin.black.raison.detre@gmail.com>
2024-04-21 05:20:23 +0800

    Added mobile layout for navbar

--- src/common/Nav/index.tsx
+++ src/common/Nav/index.tsx
@@ -1,8 +1,9 @@
+import { useState } from "preact/hooks";
+
+import clsx from "clsx";
 import { signOut } from "firebase/auth";
 
 import { auth } from "../../firebase";
-
-import clsx from "clsx";
 
 import "./index.scss";
 
@@ -12,6 +13,8 @@ type Props = {
 };
 
 export default function App({ isAuth, currRoute }: Props) {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
   const handleLogout = () => {
     signOut(auth);
   };
@@ -21,59 +24,117 @@ export default function App({ isAuth, currRoute }: Props) {
   };
 
   return (
-    <nav id="nav">
-      <a href="#" class="brand">
-        <img src="/favicon.png" />
-        <span>Fourleaf</span>
-      </a>
-      <ul class="menu">
-        <li>
-          <a
-            href={isRoute("/") ? "#" : "/"}
-            class={clsx({ active: isRoute("/") })}
-          >
-            Notes
-          </a>
-        </li>
-        <li>
-          <a
-            href={isRoute("/bills") ? "#" : "/bills"}
-            class={clsx({ active: isRoute("/bills") })}
-          >
-            Bills
-          </a>
-        </li>
-        <li>
-          <a
-            href={isRoute("/journal") ? "#" : "/journal"}
-            class={clsx({ active: isRoute("/journal") })}
-          >
-            Journal
-          </a>
-        </li>
-        <li>
-          <a
-            href={isRoute("/gas") ? "#" : "/gas"}
-            class={clsx({ active: isRoute("/gas") })}
-          >
-            Gas Monitoring
-          </a>
-        </li>
-      </ul>
-      {isAuth ? (
-        <>
-          <div class="logout-button">
-            <a href="#" onClick={handleLogout}>
-              Logout
+    <>
+      <nav id="nav">
+        <a href="#" class="brand">
+          <img src="/favicon.png" />
+          <span class="hide-sm">Fourleaf</span>
+        </a>
+
+        <ul class="menu flex hide-md">
+          <li>
+            <a
+              href={isRoute("/") ? "#" : "/"}
+              class={clsx({ active: isRoute("/") })}
+            >
+              Notes
             </a>
+          </li>
+          <li>
+            <a
+              href={isRoute("/bills") ? "#" : "/bills"}
+              class={clsx({ active: isRoute("/bills") })}
+            >
+              Bills
+            </a>
+          </li>
+          <li>
+            <a
+              href={isRoute("/journal") ? "#" : "/journal"}
+              class={clsx({ active: isRoute("/journal") })}
+            >
+              Journal
+            </a>
+          </li>
+          <li>
+            <a
+              href={isRoute("/gas") ? "#" : "/gas"}
+              class={clsx({ active: isRoute("/gas") })}
+            >
+              Gas Monitoring
+            </a>
+          </li>
+        </ul>
+
+        {isAuth ? (
+          <>
+            <div class="logout-button d-flex hide-md">
+              <a href="#" onClick={handleLogout}>
+                Logout
+              </a>
+            </div>
+            <div class="user-image d-flex hide-md">
+              <img src="/favicon.png" />
+            </div>
+            <div class="buffer hide d-flex-md"></div>
+          </>
+        ) : (
+          <a href="/login">Login</a>
+        )}
+
+        <div
+          class="hide block-md pointer mobile-menu-trigger "
+          onClick={() => setMenuOpen(true)}
+        >
+          <img src="/bars-solid.svg" />
+        </div>
+      </nav>
+      {isMenuOpen && (
+        <div id="mobile_menu" class="mobile-menu-overlay">
+          <div class="mobile-menu-close flex">
+            <img
+              src="/xmark-solid.svg"
+              alt="X"
+              class="pointer"
+              onClick={() => setMenuOpen(false)}
+            />
           </div>
-          <div class="user-image">
-            <img src="/favicon.png" />
-          </div>
-        </>
-      ) : (
-        <a href="/login">Login</a>
+          <ul class="mobile-menu">
+            <li>
+              <a
+                href={isRoute("/") ? "#" : "/"}
+                class={clsx({ active: isRoute("/") })}
+              >
+                Notes
+              </a>
+            </li>
+            <li>
+              <a
+                href={isRoute("/bills") ? "#" : "/bills"}
+                class={clsx({ active: isRoute("/bills") })}
+              >
+                Bills
+              </a>
+            </li>
+            <li>
+              <a
+                href={isRoute("/journal") ? "#" : "/journal"}
+                class={clsx({ active: isRoute("/journal") })}
+              >
+                Journal
+              </a>
+            </li>
+            <li>
+              <a
+                href={isRoute("/gas") ? "#" : "/gas"}
+                class={clsx({ active: isRoute("/gas") })}
+              >
+                Gas Monitoring
+              </a>
+            </li>
+          </ul>
+        </div>
       )}
-    </nav>
+    </>
   );
 }
