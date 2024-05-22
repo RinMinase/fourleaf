6327ab2e074515cfe2de26d371472a5738568227
Rin Minase <rin.black.raison.detre@gmail.com>
2024-05-22 13:38:01 +0800

    Fixed table semantics

--- src/gas/components/FuelTable.tsx
+++ src/gas/components/FuelTable.tsx
@@ -9,56 +9,59 @@ export default function FuelTable() {
             <th class="px-3 py-">Tank Percent</th>
           </tr>
         </thead>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">Empty</td>
-          <td class="px-3 py-2">3</td>
-          <td class="px-3 py-2">12%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">1 bar</td>
-          <td class="px-3 py-2">6</td>
-          <td class="px-3 py-2">23%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">2 bars</td>
-          <td class="px-3 py-2">9</td>
-          <td class="px-3 py-2">35%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">3 bars</td>
-          <td class="px-3 py-2">11</td>
-          <td class="px-3 py-2">42%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">4 bars</td>
-          <td class="px-3 py-2">14</td>
-          <td class="px-3 py-2">54%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">5 bars</td>
-          <td class="px-3 py-2">17</td>
-          <td class="px-3 py-2">65%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">6 bars</td>
-          <td class="px-3 py-2">20</td>
-          <td class="px-3 py-2">77%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">7 bars</td>
-          <td class="px-3 py-2">23</td>
-          <td class="px-3 py-2">88%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">8 bars</td>
-          <td class="px-3 py-2">26</td>
-          <td class="px-3 py-2">100%</td>
-        </tr>
-        <tr class="even:bg-gray-200">
-          <td class="px-3 py-2">8+ bars</td>
-          <td class="px-3 py-2">28</td>
-          <td class="px-3 py-2">108%</td>
-        </tr>
+
+        <tbody>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">Empty</td>
+            <td class="px-3 py-2">3</td>
+            <td class="px-3 py-2">12%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">1 bar</td>
+            <td class="px-3 py-2">6</td>
+            <td class="px-3 py-2">23%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">2 bars</td>
+            <td class="px-3 py-2">9</td>
+            <td class="px-3 py-2">35%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">3 bars</td>
+            <td class="px-3 py-2">11</td>
+            <td class="px-3 py-2">42%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">4 bars</td>
+            <td class="px-3 py-2">14</td>
+            <td class="px-3 py-2">54%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">5 bars</td>
+            <td class="px-3 py-2">17</td>
+            <td class="px-3 py-2">65%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">6 bars</td>
+            <td class="px-3 py-2">20</td>
+            <td class="px-3 py-2">77%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">7 bars</td>
+            <td class="px-3 py-2">23</td>
+            <td class="px-3 py-2">88%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">8 bars</td>
+            <td class="px-3 py-2">26</td>
+            <td class="px-3 py-2">100%</td>
+          </tr>
+          <tr class="even:bg-gray-200">
+            <td class="px-3 py-2">8+ bars</td>
+            <td class="px-3 py-2">28</td>
+            <td class="px-3 py-2">108%</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
