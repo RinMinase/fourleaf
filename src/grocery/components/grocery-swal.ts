69a2adf4839992cf8b34cb544102c43c293d9629
Rin Minase <rin.black.raison.detre@gmail.com>
2024-09-14 00:20:39 +0800

    Updated dialog config for grocery list

--- src/grocery/components/grocery-swal.ts
+++ src/grocery/components/grocery-swal.ts
@@ -4,6 +4,10 @@ const Swal = SweetAlert.mixin({
   showClass: {
     popup: "animate__animated animate__fadeInDown",
   },
+  customClass: {
+    title: "!text-2xl",
+  },
+  confirmButtonColor: "#28A745",
   allowOutsideClick: false,
 });
 
