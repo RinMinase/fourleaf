2ec3929d0796a25ee430fbd386ac77dd5dec1bf9
Rin Minase <rin.black.raison.detre@gmail.com>
2024-09-13 11:21:57 +0800

    Added firebase db env config

--- src/firebase.ts
+++ src/firebase.ts
@@ -11,6 +11,7 @@ if (
   const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
     authDomain: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.firebaseapp.com`,
     storageBucket: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}.appspot.com`,
   };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_FIREBASE_API_KEY: string;
   readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_DB_URL: string;
 }
 
 interface ImportMeta {
