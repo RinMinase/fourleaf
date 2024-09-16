44ba6cc1fa47a53e7d3ceb166ebab841aa34233f
Rin Minase <rin.black.raison.detre@gmail.com>
2024-09-16 18:43:09 +0800

    Removed usage of es2023

--- src/grocery/components/sort-categories.ts
+++ src/grocery/components/sort-categories.ts
@@ -1,7 +1,7 @@
 import { Category } from "../types";
 
 export const sortCategories = (lists: Array<Category>): Array<Category> => {
-  return lists.toSorted((a, b) => {
+  return lists.sort((a, b) => {
     if (a.order && b.order) return a.order - b.order;
     return 1;
   });
