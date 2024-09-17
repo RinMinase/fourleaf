a5a05a4f5ca419979021d83e583f09deaa7b2f16
Rin Minase <rin.black.raison.detre@gmail.com>
2024-09-17 18:17:25 +0800

    Modularized list container

---
+++ src/grocery/components/desktop-list-container.tsx
@@ -0,0 +1,90 @@
+import { Dispatch, MutableRef, StateUpdater } from "preact/hooks";
+
+import { push, Unsubscribe, update } from "firebase/database";
+
+import { groceryDB } from "./db";
+import ListTile from "./desktop-list-tile";
+import Swal from "./grocery-swal";
+import { ListItem } from "../types";
+
+type Props = {
+  lists: Array<ListItem>;
+  isListLoading: boolean;
+  setListData: Dispatch<StateUpdater<ListItem>>;
+  setListDataLoading: Dispatch<StateUpdater<boolean>>;
+  setIsCollapseOpen: Dispatch<StateUpdater<boolean[]>>;
+  listItemSubscription: MutableRef<Unsubscribe | undefined>;
+};
+
+export default function ListContainer(props: Props) {
+  const handleAddList = async () => {
+    const today = new Date().toISOString().substring(0, 10);
+
+    const { value: formValues } = await Swal.fire({
+      title: "Add List",
+      showCancelButton: true,
+      html: `
+        <input id="swal-input1" placeholder="Name" class="w-full border border-slate-300 rounded px-2 py-1.5 !mb-4 text-sm h-9 shadow-none mt-3">
+        <input id="swal-input2" type="date" class="w-full border border-slate-300 rounded px-2 py-1.5 text-sm h-9 shadow-none mt-3" value="${today}">`,
+      focusConfirm: false,
+      preConfirm: () => {
+        const name = (document.getElementById("swal-input1") as any).value;
+        const date = (document.getElementById("swal-input2") as any).value;
+
+        if (!name) {
+          Swal.showValidationMessage("Name should not be blank");
+          document
+            .getElementById("swal-input1")
+            ?.classList.add("!border-red-400");
+        }
+
+        return [name, date];
+      },
+    });
+
+    if (formValues) {
+      const [name, date] = formValues;
+      const newKey = push(groceryDB).key;
+
+      update(groceryDB, {
+        [`${newKey}`]: {
+          id: newKey,
+          name,
+          date,
+          list: [],
+        },
+      });
+    }
+  };
+
+  return (
+    <>
+      <div class="flex">
+        <h1 class="grow text-xl">Lists</h1>
+
+        <button
+          class="!text-xs font-medium uppercase bg-green-400 hover:bg-green-300 px-4 py-0.5 rounded cursor-pointer"
+          onClick={handleAddList}
+        >
+          Add List
+        </button>
+      </div>
+
+      <div class="overflow-y-auto mt-3 border border-slate-400 rounded-sm h-[calc(100%-28px-24px)]">
+        {props.isListLoading ? (
+          <div class="spinner loader" />
+        ) : (
+          props.lists.map((list) => (
+            <ListTile
+              setListData={props.setListData}
+              setListDataLoading={props.setListDataLoading}
+              setIsCollapseOpen={props.setIsCollapseOpen}
+              listItemSubscription={props.listItemSubscription}
+              list={list}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
+}
--- src/grocery/desktop.tsx
+++ src/grocery/desktop.tsx
@@ -1,18 +1,12 @@
 import { useEffect, useRef, useState } from "preact/hooks";
 import { checkDeviceIfMobile } from "../common/functions";
 
-import { ListItem as ListItemType } from "./types";
-import {
-  child,
-  onValue,
-  push,
-  remove,
-  Unsubscribe,
-  update,
-} from "firebase/database";
+import { child, onValue, remove, Unsubscribe } from "firebase/database";
+
 import { groceryDB } from "./components/db";
-import ListTile from "./components/desktop-list-tile";
 import Swal from "./components/grocery-swal";
+import ListContainer from "./components/desktop-list-container";
+import { ListItem as ListItemType } from "./types";
 
 const isMobile = checkDeviceIfMobile();
 
@@ -43,46 +37,6 @@ export default function App() {
 
       setListLoading(false);
     });
-  };
-
-  const handleAddList = async () => {
-    const today = new Date().toISOString().substring(0, 10);
-
-    const { value: formValues } = await Swal.fire({
-      title: "Add List",
-      showCancelButton: true,
-      html: `
-        <input id="swal-input1" placeholder="Name" class="w-full border border-slate-300 rounded px-2 py-1.5 !mb-4 text-sm h-9 shadow-none mt-3">
-        <input id="swal-input2" type="date" class="w-full border border-slate-300 rounded px-2 py-1.5 text-sm h-9 shadow-none mt-3" value="${today}">`,
-      focusConfirm: false,
-      preConfirm: () => {
-        const name = (document.getElementById("swal-input1") as any).value;
-        const date = (document.getElementById("swal-input2") as any).value;
-
-        if (!name) {
-          Swal.showValidationMessage("Name should not be blank");
-          document
-            .getElementById("swal-input1")
-            ?.classList.add("!border-red-400");
-        }
-
-        return [name, date];
-      },
-    });
-
-    if (formValues) {
-      const [name, date] = formValues;
-      const newKey = push(groceryDB).key;
-
-      update(groceryDB, {
-        [`${newKey}`]: {
-          id: newKey,
-          name,
-          date,
-          list: [],
-        },
-      });
-    }
   };
 
   const handleDeleteList = async (id: string) => {
@@ -132,32 +86,14 @@ export default function App() {
     <div class="flex max-h-full h-full">
       <div class="w-1/3 lg:w-1/4 flex flex-col">
         <div class="h-1/2 pr-2">
-          <div class="flex">
-            <h1 class="grow text-xl">Lists</h1>
-
-            <button
-              class="!text-xs font-medium uppercase bg-green-400 hover:bg-green-300 px-4 py-0.5 rounded cursor-pointer"
-              onClick={handleAddList}
-            >
-              Add List
-            </button>
-          </div>
-
-          <div class="overflow-y-auto mt-3 border border-slate-400 rounded-sm h-[calc(100%-28px-24px)]">
-            {isListLoading ? (
-              <div class="spinner loader" />
-            ) : (
-              lists.map((list) => (
-                <ListTile
-                  setListData={setListData}
-                  setListDataLoading={setListDataLoading}
-                  setIsCollapseOpen={setIsCollapseOpen}
-                  listItemSubscription={listItemSubscription}
-                  list={list}
-                />
-              ))
-            )}
-          </div>
+          <ListContainer
+            lists={lists}
+            isListLoading={isListLoading}
+            setListData={setListData}
+            setListDataLoading={setListDataLoading}
+            setIsCollapseOpen={setIsCollapseOpen}
+            listItemSubscription={listItemSubscription}
+          />
         </div>
         <div class="h-1/2">
           <h1 class="text-xl">Order Categories</h1>
