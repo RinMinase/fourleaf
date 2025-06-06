a9d9eaf6944e13a167a2262168078066d95c19e3
Rin Minase <rin.black.raison.detre@gmail.com>
2025-06-06 17:17:02 +0800

    Added api key to request

--- src/http.ts
+++ src/http.ts
@@ -9,12 +9,14 @@ const removeTrailSlashes = (str: string): string => {
 };
 
 let API_URL = import.meta.env.VITE_API_URL || "";
+let API_KEY = import.meta.env.VITE_API_KEY || "";
 
 if (API_URL) {
   API_URL = removeTrailSlashes(API_URL);
 
   axios.defaults.baseURL = `${API_URL}/api/fourleaf`;
   axios.defaults.headers.post["Content-Type"] = "application/json";
+  axios.defaults.headers.common["x-api-key"] = API_KEY;
   axios.defaults.withCredentials = true;
 
   axios.interceptors.response.use(
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,9 @@ interface ImportMetaEnv {
   readonly VITE_FIREBASE_API_KEY: string;
   readonly VITE_FIREBASE_PROJECT_ID: string;
   readonly VITE_FIREBASE_DB_URL: string;
+
+  readonly VITE_API_URL: string;
+  readonly VITE_API_KEY: string;
 }
 
 interface ImportMeta {
